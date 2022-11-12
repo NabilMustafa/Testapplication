8b28af159df5fed42f40dca99fb199a8a448f43a
NabilMustafa <muhdnabilmustafa@gmail.com>
2022-11-13 04:30:30 +0800

    fix lab 2

--- app/src/main/java/ftmk/bitp3453/helloclass/FirstActivity.java
+++ app/src/main/java/ftmk/bitp3453/helloclass/FirstActivity.java
@@ -2,6 +2,7 @@ package ftmk.bitp3453.helloclass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,12 +22,23 @@ public class FirstActivity extends AppCompatActivity {
 
         txtvwAge = (TextView) findViewById(R.id.txtvwAge);
         edtName = (EditText) findViewById(R.id.edtTxtName);
-        edtName = (EditText) findViewById(R.id.edtYear);
+        edtYear = (EditText) findViewById(R.id.edtYear);
 
     }
     public void fnGreet(View vw)
     {
-        String strName = edtName.getText() .toString();
-        txtvwAge.setText("Hellooo and welcome " + strName);
+        String strDate = edtYear.getText().toString();
+        int number = Integer.parseInt(strDate);
+        int strAge = (2022 - number);
+
+        String strName = edtName.getText().toString();
+        txtvwAge.setText("Hellooo and welcome " + strName +"."+" You are "+ strAge);
+    }
+    public void fnThreadActivity(View vw)
+    {
+        Intent intent = new Intent(this,ThreadedActivity.class);
+        String strMsg = edtName.getText().toString();
+        intent.putExtra("varStr1",strMsg);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/ftmk/bitp3453/helloclass/StudentMainActivity.java
@@ -0,0 +1,14 @@
+package ftmk.bitp3453.helloclass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_main);
+    }
+}
