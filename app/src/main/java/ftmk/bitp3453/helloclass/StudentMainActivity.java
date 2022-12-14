ef7eb8564341737729b5fbab04d5437f14bcac02
NabilMustafa <muhdnabilmustafa98@gmail.com>
2022-12-15 03:43:01 +0800

    just maybe

--- app/src/main/java/ftmk/bitp3453/helloclass/StudentMainActivity.java
+++ app/src/main/java/ftmk/bitp3453/helloclass/StudentMainActivity.java
@@ -1,14 +1,135 @@
 package ftmk.bitp3453.helloclass;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import ftmk.bitp3453.helloclass.databinding.ActivityStudentMainBinding;
+
+import java.util.Calendar;
+import java.util.Vector;
 
 public class StudentMainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    private ActivityStudentMainBinding binding;
+    private Student student;
+    private Vector<Student> students;
+    private StudentAdapter adapter;
+    private DatePickerDialog datePicker;
+    LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student_main);
+        linearLayout = findViewById(R.id.linear_layout);
+
+        binding = ActivityStudentMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.fabAdd.setOnClickListener(this:: fnAdd );
+
+        binding.edtBirthdate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View view, boolean b)
+            {
+                if(b) {
+                    fnInvokeDatePicker();
+                }
+                if(!b)
+                {
+                    fnFormValidaton();
+                }
+
+            }
+
+        });
+
+        binding.edtBirthdate.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                fnInvokeDatePicker();
+            }
+        });
+
+        students = new Vector<>();
+        adapter = new StudentAdapter(getLayoutInflater(),students);
+
+
+        binding.rcvStud.setAdapter(adapter);
+        binding.rcvStud.setLayoutManager(new LinearLayoutManager(this));
+
+        ItemTouchHelper helper = new ItemTouchHelper(callback);
+        helper.attachToRecyclerView(binding.rcvStud);
+
     }
+
+    ItemTouchHelper.SimpleCallback callback = new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT|ItemTouchHelper.RIGHT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+
+            students.remove(viewHolder.getAdapterPosition());
+            adapter.notifyDataSetChanged();
+        }
+    };
+
+    private void fnFormValidaton() {
+    }
+
+    private void fnInvokeDatePicker()
+    {
+        final Calendar cldr = Calendar.getInstance();
+        int day = cldr.get(Calendar.DAY_OF_MONTH);
+        int month = cldr.get(Calendar.MONTH);
+        int year = cldr.get(Calendar.YEAR);
+        // date picker dialog
+
+        datePicker = new DatePickerDialog(StudentMainActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                binding.edtBirthdate.setText(dayOfMonth + "/" + (month+1) + "/" + year);
+            }
+        },year,month,day);
+        datePicker.show();
+    }
+
+    private void fnAdd(View view)
+    {
+        String fullname = binding.edtFullName.getText().toString();
+        String studNo = binding.edtStudNum.getText().toString();
+        String email = binding.edtEmail.getText().toString();
+        String birth = binding.edtBirthdate.getText().toString();
+        String gender = "";
+        String state = binding.spnState.getSelectedItem().toString();
+
+        if(binding.rbMale.isChecked())
+            gender = binding.rbMale.getText().toString();
+        else if(binding.rbFemale.isChecked())
+            gender = binding.rbFemale.getText().toString();
+
+        student = new Student(fullname,studNo,email,gender,birth, state);
+
+        students.add(student);
+        adapter.notifyItemInserted(students.size());
+
+
+
+    }
+
+
 }
